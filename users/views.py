7fd635b71bcfc06a3379b481d56e5162fe8692ba
KafetzisThomas <tomkafetzis06@gmail.com>
2024-08-26 10:32:25 +0300

    Display account-related messages to users

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UpdateUserForm, UpdateProfileForm
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def register(request):
@@ -14,6 +15,9 @@ def register(request):
         form = UserCreationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(
+                request, "Account successfully created! You're now able to login."
+            )
             return redirect("users:login")
     else:
         form = UserCreationForm()
@@ -35,6 +39,9 @@ def account(request):
             update_session_auth_hash(
                 request, request.user
             )  # Important for keeping the user logged in
+            messages.success(
+                request, "Your account settings were successfully updated!"
+            )
             return redirect("rt_chat:chat_list")
     else:
         user_form = UpdateUserForm(instance=request.user)
@@ -48,4 +55,5 @@ def account(request):
 def delete_account(request):
     user = User.objects.get(id=request.user.id)
     user.delete()
+    messages.error(request, "Your account has been successfully deleted!")
     return redirect("users:register")
