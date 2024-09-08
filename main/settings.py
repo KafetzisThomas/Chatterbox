e1e6d10c5f63ea6aa595a573036212060eb15886
KafetzisThomas <tomkafetzis06@gmail.com>
2024-09-08 12:35:39 +0300

    Update ALLOWED_HOSTS

--- main/settings.py
+++ main/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "").lower() == "true"
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1:8000", "chatterbox-demo.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "chatterbox-demo.onrender.com"]
 
 CSRF_TRUSTED_ORIGINS = ["https://chatterbox-demo.onrender.com"]
 
