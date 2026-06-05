80b6228e5e22e1a1a8ee2e88cca75f2e7d7b2462
KafetzisThomas <tomkafetzis06@gmail.com>
2026-06-05 15:46:18 +0300

    Update url patterns for apps

--- users/urls.py
+++ users/urls.py
@@ -1,23 +1,12 @@
-"""Defines URL patterns for users"""
-
-from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = "users"
 urlpatterns = [
-    # Login page
-    path(
-        "login/",
-        LoginView.as_view(template_name="registration/login.html"),
-        name="login",
-    ),
-    # Registration page
     path("register/", views.register, name="register"),
-    # Account page
+    path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
     path("account/", views.account, name="account"),
-    # Delete account page
-    path("account/delete_account/", views.delete_account, name="delete_account"),
-    # Logout page
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path("account/delete/", views.delete_account, name="delete_account"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
