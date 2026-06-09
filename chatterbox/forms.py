71b5c89c2552d2d364c2b5102e276cf7deb58e28
KafetzisThomas <tomkafetzis06@gmail.com>
2026-06-09 17:42:44 +0300

    Add autofocus to username input in chat form

--- chatterbox/forms.py
+++ chatterbox/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class PrivateChatForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(
-        attrs={"class": "form-control", "placeholder": "Enter username to chat"}), max_length=100,
+        attrs={"class": "form-control", "placeholder": "Enter username to chat", "autofocus": True}), max_length=100,
     )
